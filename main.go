c22a7a2776b81fc1a186a6f71647943722802f1e
Daaaai0809 <dai.tsuruga@80and.co>
2025-07-01 20:11:07 +0900

    feat : envの取得とenv checkの実装 #5

--- main.go
+++ main.go
@@ -3,8 +3,29 @@ Copyright © 2025 NAME HERE dai.tsuruga0809@gmail.com
 */
 package main
 
-import "github.com/Daaaai0809/swagen-v2/cmd"
+import (
+	"fmt"
+	"os"
+
+	"github.com/Daaaai0809/swagen-v2/cmd"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	godotenv.Load(".env")
+	if err := validateEnv(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
 	cmd.Execute()
+}
+
+func validateEnv() error {
+	requiredVars := []string{"MODEL_PATH", "SCHEME_PATH", "API_PATH"}
+	for _, envVar := range requiredVars {
+		if value := os.Getenv(envVar); value == "" {
+			return fmt.Errorf("required environment variable %s is not set", envVar)
+		}
+	}
+	return nil
 }
