7d5bb9b83edacc33520eab8b4761600c90271344
Daaaai0809 <dai.tsuruga@80and.co>
2025-07-01 17:15:48 +0900

    feat : type, formatの定義 #5

---
+++ constants/fieldTypes.go
@@ -0,0 +1,19 @@
+package constants
+
+const (
+	STRING_TYPE  = "string"
+	NUMBER_TYPE  = "number"
+	INTEGER_TYPE = "integer"
+	BOOLEAN_TYPE = "boolean"
+	ARRAY_TYPE   = "array"
+	OBJECT_TYPE  = "object"
+)
+
+var FieldTypeList = []string{
+	STRING_TYPE,
+	NUMBER_TYPE,
+	INTEGER_TYPE,
+	BOOLEAN_TYPE,
+	ARRAY_TYPE,
+	OBJECT_TYPE,
+}
---
+++ constants/formats.go
@@ -0,0 +1,65 @@
+package constants
+
+const (
+	FORMAT_DATE                  = "date"
+	FORMAT_DATE_TIME             = "date-time"
+	FORMAT_PASSWORD              = "password"
+	FORMAT_BYTE                  = "byte"
+	FORMAT_BINARY                = "binary"
+	FORMAT_EMAIL                 = "email"
+	FORMAT_UUID                  = "uuid"
+	FORMAT_HOSTNAME              = "hostname"
+	FORMAT_IPV4                  = "ipv4"
+	FORMAT_IPV6                  = "ipv6"
+	FORMAT_URI                   = "uri"
+	FORMAT_URI_REF               = "uri-reference"
+	FORMAT_URI_TEMPLATE          = "uri-template"
+	FORMAT_JSON_POINTER          = "json-pointer"
+	FORMAT_RELATIVE_JSON_POINTER = "relative-json-pointer"
+	FORMAT_REGEX                 = "regex"
+)
+
+var FormatStringList = []string{
+	FORMAT_DATE,
+	FORMAT_DATE_TIME,
+	FORMAT_PASSWORD,
+	FORMAT_BYTE,
+	FORMAT_BINARY,
+	FORMAT_EMAIL,
+	FORMAT_UUID,
+	FORMAT_HOSTNAME,
+	FORMAT_IPV4,
+	FORMAT_IPV6,
+	FORMAT_URI,
+	FORMAT_URI_REF,
+	FORMAT_URI_TEMPLATE,
+	FORMAT_JSON_POINTER,
+	FORMAT_RELATIVE_JSON_POINTER,
+	FORMAT_REGEX,
+}
+
+const (
+	FORMAT_FLOAT  = "float"
+	FORMAT_DOUBLE = "double"
+)
+
+var FormatNumberList = []string{
+	FORMAT_FLOAT,
+	FORMAT_DOUBLE,
+}
+
+const (
+	FORMAT_INT32 = "int32"
+	FORMAT_INT64 = "int64"
+)
+
+var FormatIntegerList = []string{
+	FORMAT_INT32,
+	FORMAT_INT64,
+}
+
+var FormatList = map[string][]string{
+	STRING_TYPE:  FormatStringList,
+	NUMBER_TYPE:  FormatNumberList,
+	INTEGER_TYPE: FormatIntegerList,
+}
