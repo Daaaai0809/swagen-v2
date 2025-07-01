50fadee9bc3d6da02caef41acd97937f19249897
Daaaai0809 <dai.tsuruga@80and.co>
2025-07-01 17:15:30 +0900

    fix : 不要な説明削除, root command名変更 #5

--- cmd/root.go
+++ cmd/root.go
@@ -11,14 +11,10 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "help",
-	Short: "The Usage of this cli application",
+	Use:   "swagen",
 	Long: `
-	This is a CLI application that helps your OpenAPI schema definition.
-	You can generate API endpoint schemas, models, and other related files.
-	
-	[COMMAND]
-	- get: Generate GET API endpoint schema
+This is a CLI application that helps your OpenAPI schema definition.
+You can generate API endpoint schemas, models, and other related files.
 `,
 }
 
