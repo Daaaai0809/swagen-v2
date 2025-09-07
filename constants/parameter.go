89363a3ce287c4e4b2bbb053f3e8012ddfef8d6c
Daaaai0809 <dai.tsuruga@80and.co>
2025-09-07 18:36:22 +0900

    feat : add mediaType / Param In constants #6

---
+++ constants/parameter.go
@@ -0,0 +1,17 @@
+package constants
+
+const (
+	PARAM_IN_PATH = "path"
+	PARAM_IN_QUERY = "query"
+	PARAM_IN_HEADER = "header"
+	PARAM_IN_COOKIE = "cookie"
+	PARAM_IN_BODY = "body"
+)
+
+var ReflableParamIn = []string{
+	PARAM_IN_PATH,
+	PARAM_IN_QUERY,
+	PARAM_IN_HEADER,
+	PARAM_IN_COOKIE,
+	PARAM_IN_BODY,
+}
